2c9eaf7a3729eb2ccd59012105b367626669074d
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-12 14:36:10 -0300

    Update ex_05.py

--- poo/medio/ex_05.py
+++ poo/medio/ex_05.py
@@ -5,11 +5,11 @@
 
 # classmethod: Crie um classmethod chamado criar_agora(nome) que:
 
-# Usa o módulo datetime para obter o timestamp atual (datetime.now()).
+# Usa o módulo datetime para obter o timestamp atual (datetime.now()). ✔️
 
-# Cria e retorna uma nova instância de RegistroHistorico com o nome fornecido e o timestamp atual.
+# Cria e retorna uma nova instância de RegistroHistorico com o nome fornecido e o timestamp atual. ✔️
 
-# Propósito: Demonstrar como usar classmethods como construtores alternativos, úteis para inicializar objetos de diferentes formas.
+# Propósito: Demonstrar como usar classmethods como construtores alternativos, úteis para inicializar objetos de diferentes formas. ✔️
 
 import datetime
 
