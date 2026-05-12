e0a3d7c549d258fbffb9bbe4f74b3abf849198c7
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-12 14:06:49 -0300

    Create ex_05.py

---
+++ poo/medio/ex_05.py
@@ -0,0 +1,12 @@
+# Exercício 5: Classe RegistroHistorico (Classmethod - Construtor Alternativo)
+# Crie a classe RegistroHistorico com atributos nome (string) e timestamp (datetime).
+
+# Método __init__: Aceita nome e timestamp.
+
+# classmethod: Crie um classmethod chamado criar_agora(nome) que:
+
+# Usa o módulo datetime para obter o timestamp atual (datetime.now()).
+
+# Cria e retorna uma nova instância de RegistroHistorico com o nome fornecido e o timestamp atual.
+
+# Propósito: Demonstrar como usar classmethods como construtores alternativos, úteis para inicializar objetos de diferentes formas.
