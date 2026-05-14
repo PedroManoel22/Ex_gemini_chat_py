79e118639e6278ccb344f977eca20ac334ecce3c
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-14 08:40:43 -0300

    Created ex_10.py

---
+++ poo/medio/ex_10.py
@@ -0,0 +1,12 @@
+# Exercício 10: Herança e Sobrescrita de Métodos (__str__)
+# Crie as seguintes classes:
+
+# Animal: Classe base com atributos nome e especie. Deve ter um método fazer_som().
+
+# Cachorro (Herda de Animal):
+
+# Inicializa especie como "Canina".
+
+# Sobrescreve fazer_som() para retornar "Latido".
+
+# Sobrescreve o método mágico __str__ para retornar uma descrição amigável (ex: "Cachorro de nome Rex, espécie Canina").
