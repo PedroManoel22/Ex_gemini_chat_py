d0d2c69b40f428860187a392fbf7c79d6977e3f6
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-14 08:30:43 -0300

    Created ex_09.py

---
+++ poo/medio/ex_09.py
@@ -0,0 +1,10 @@
+# Exercício 9: Classe Pessoa com Controle de Idade (Property com Setter)
+# Crie a classe Pessoa com os atributos nome (string) e _idade (int, privado por convenção).
+
+# @property (Getter): Para o atributo idade.
+
+# @idade.setter (Setter): Permite a alteração da idade, mas deve validar que:
+
+# A nova idade é um número positivo (> 0).
+
+# Se a validação falhar, levante uma exceção (ValueError).
