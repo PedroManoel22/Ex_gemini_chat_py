0411a42df1e8af52e05f4e5c3162cc0e836e2ec2
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-14 08:39:50 -0300

    feat: implement age control with property and setter in Pessoa class

--- poo/medio/ex_09.py
+++ poo/medio/ex_09.py
@@ -8,3 +8,32 @@
 # A nova idade é um número positivo (> 0).
 
 # Se a validação falhar, levante uma exceção (ValueError).
+
+
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self._idade = idade
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.getter
+    def idade(self):
+        return f"Idade: {self._idade}"
+
+    @idade.setter
+    def idade(self, nova_idade: int):
+        if nova_idade > 0:
+            self._idade = nova_idade
+
+        else:
+            raise ValueError("Por favor insira uma idade maior que 0!")
+
+
+p1 = Pessoa("Pedro", 21)
+print(p1.idade)
+p1.idade = 2
+print(p1.idade)
+p1.idade = 0
