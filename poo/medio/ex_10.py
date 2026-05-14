3c9c9ed2a5709f0e3e165b8fb1d58ccacb70cd54
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-14 08:53:37 -0300

    Resolution ex_10.py

--- poo/medio/ex_10.py
+++ poo/medio/ex_10.py
@@ -10,3 +10,28 @@
 # Sobrescreve fazer_som() para retornar "Latido".
 
 # Sobrescreve o método mágico __str__ para retornar uma descrição amigável (ex: "Cachorro de nome Rex, espécie Canina").
+
+
+class Animal:
+    def __init__(self, nome: str) -> None:
+        self.nome = nome
+        self.especie = ""
+
+    def fazer_som(self) -> str: ...
+
+
+class Cachorro(Animal):
+    def __init__(self, nome: str) -> None:
+        super().__init__(nome)
+        self.especie = "Canina"
+
+    def fazer_som(self) -> str:
+        return "Latido"
+
+    def __str__(self) -> str:
+        return f"Cachorro de nome {self.nome}, espécie {self.especie}"
+
+
+c1 = Cachorro("Rex")
+print(c1)
+print(c1.fazer_som())
